d8640c8c0530d9411409c9da6385bbe0b7397e2b
NanZ-hash <afnanxmt@gmail.com>
2020-04-07 16:11:16 +0300

    // Authentication for the SIGN IN and SIGN UP , SIGN OUT  from the template provided..

---
+++ src/auth/api.js
@@ -0,0 +1,56 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const signUp = credentials => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/sign-up',
+    data: {
+      credentials: {
+        username: credentials.username,
+        email: credentials.email,
+        password: credentials.password,
+        password_confirmation: credentials.passwordConfirmation
+      }
+    }
+  })
+}
+
+export const signIn = credentials => {
+  return axios({
+    url: apiUrl + '/sign-in',
+    method: 'POST',
+    data: {
+      credentials: {
+        email: credentials.email,
+        password: credentials.password
+      }
+    }
+  })
+}
+
+export const signOut = user => {
+  return axios({
+    url: apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+     'Authorization': `Token ${user.token}` // FOR RAILS
+    }
+  })
+}
+
+export const changePassword = (passwords, user) => {
+  return axios({
+    url: apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Token ${user.token}` // FOR RAILS
+    },
+    data: {
+      passwords: {
+        old: passwords.oldPassword,
+        new: passwords.newPassword
+      }
+    }
+  })
+}
