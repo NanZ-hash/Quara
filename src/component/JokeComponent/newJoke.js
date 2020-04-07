b0dc85f2c2576314138f6264d676ea1bfd435516
NanZ-hash <afnanxmt@gmail.com>
2020-04-08 01:33:23 +0300

    creating CREATE method for the JOKE component to creating new jokes

---
+++ src/component/JokeComponent/newJoke.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+ const NewJoke = ({newJokeSubmit = e => e}) => {
+    let content
+    const submit = e => {
+        e.preventDefault()
+        newJokeSubmit(content.value)
+        content.value = ''
+    }
+
+    return (
+        <form onSubmit={submit}>
+                    <div>
+            <textarea ref={textarea => content = textarea}
+                    type="textarea"
+                    placeholder="Tell me .." required/>
+                    </div>
+
+            <button>Add Joke</button>
+        </form>
+    )
+}
+
+export default NewJoke
