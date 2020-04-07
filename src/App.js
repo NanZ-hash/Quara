ef1f1526c84c385a9d3b4b479c63aef8b2b02141
NanZ-hash <afnanxmt@gmail.com>
2020-04-07 16:00:23 +0300

    // Authentication from the template provided..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import { Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Authentication from the template provided.. 
+import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
+import Header from './header/Header'
+import SignUp from './auth/components/SignUp'
+import SignIn from './auth/components/SignIn'
+import SignOut from './auth/components/SignOut'
+import ChangePassword from './auth/components/ChangePassword'
+import AlertDismissible from './auth/components/AlertDismissible'
+
+
+
+
+class App extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      user: null,
+      alerts: []
+    }
+  }
+
+  setUser = user => this.setState({ user })
+
+  clearUser = () => this.setState({ user: null })
+
+  alert = (message, type) => {
+    this.setState({ alerts: [...this.state.alerts, { message, type }] })
+  }
+
+  render () {
+    const { alerts, user } = this.state
+
+    return (
+      <React.Fragment>
+        <Header user={user} />
+        {alerts.map((alert, index) => (
+          <AlertDismissible key={index} variant={alert.type} message={alert.message} />
+        ))}
+        <main className="container">
+          <Route path='/sign-up' render={() => (
+            <SignUp alert={this.alert} setUser={this.setUser} />
+          )} />
+          <Route path='/sign-in' render={() => (
+            <SignIn alert={this.alert} setUser={this.setUser} />
+          )} />
+          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/change-password' render={() => (
+            <ChangePassword alert={this.alert} user={user} />
+          )} />
+          {/* <AuthenticatedRoute user={user} path='/main' render={() => (
+             <the user route ,, for the main component  />
+          )} /> */}
+        </main>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
