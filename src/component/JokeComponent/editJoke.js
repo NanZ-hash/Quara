430b8e69271f1e4d47a6dae9ef6179006374e115
NanZ-hash <afnanxmt@gmail.com>
2020-04-09 03:38:11 +0300

    adding a pop model to both edit components

--- src/component/JokeComponent/editJoke.js
+++ src/component/JokeComponent/editJoke.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
 
 export default class EditJoke extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             id: this.props.id,
-            content: this.props.content
+            content: this.props.content,
+            setShow: false, 
+            show:false
         }
     }
-
+ 
+        handleClose = () =>{ 
+        this.setState( { show : false })
+       } 
+        handleShow = () => { 
+           this.setState( { show : true })
+       }
+    
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
@@ -25,16 +38,37 @@ export default class EditJoke extends React.Component {
 
     render(){
         return(
-            <form>
-                <div>
-            <textarea  name="content"
-                    type="text"
-                    placeholder="Content"
+            <>
+            <Button variant="primary" onClick={this.handleShow}>
+              Edit
+            </Button>
+            <Modal show={this.state.show} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Joke</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+              <Form>
+                    <Form.Label>Content : </Form.Label>
+                <Form.Control name="content" 
+                    as="textarea" rows="3" 
+                    placeholder="..." 
                     value={this.state.content}
-                    onChange={this.handleChange} />
-                    </div>
-            <button onClick={this.editJoke}>Update Joke</button>
-        </form>  
+                    onChange={this.handleChange}
+                    />
+                    </Form> 
+
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={this.editJoke}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
+    
         )
     }
 }
--- src/component/StoryComponent/editStory.js
+++ src/component/StoryComponent/editStory.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+
 export default class EditStory extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             id: this.props.id,
             title: this.props.title,
-            content: this.props.content
+            content: this.props.content,
+            setShow: false, 
+            show:false
         }
     }
+
+    
+    handleClose = () =>{ 
+    this.setState( { show : false })
+   } 
+    handleShow = () => { 
+       this.setState( { show : true })
+   }
+
 
     handleChange = (e) => {
         this.setState({
@@ -27,23 +43,45 @@ export default class EditStory extends React.Component {
 
     render(){
         return(
-            <form>
-                <div>
-            <input  name="title"
-                    type="text"
-                    placeholder="Title..."
+            <>
+            <Button variant="primary" onClick={this.handleShow}>
+              Edit
+            </Button>
+            <Modal show={this.state.show} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Story</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+              <Form>
+                <Form.Label>Title : </Form.Label>
+                <Form.Control name="title" 
+                    type="text" 
+                    placeholder="Title..." 
                     value={this.state.title}
-                    onChange={this.handleChange} />
-                    </div>
-                    <div>
-            <textarea  name="content"
-                    type="text"
-                    placeholder="Content"
+                    onChange={this.handleChange}
+                    />
+                    <Form.Label>Content : </Form.Label>
+                <Form.Control name="content" 
+                    as="textarea" rows="3" 
+                    placeholder="..." 
                     value={this.state.content}
-                    onChange={this.handleChange} />
-                    </div>
-                    <button onClick={this.editStory}>Update Story</button>
-        </form>  
+                    onChange={this.handleChange}
+                    />
+                    </Form> 
+
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={this.editStory}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
+
+             
         )
     }
 }
